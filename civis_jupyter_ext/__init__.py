af34fadd9c565c6e916b9476cba21c160726e482
jacksonlee-civis <jacksonlee@civisanalytics.com>
2022-03-03 12:04:01 -0500

    VER bump version to v1.1.0

--- civis_jupyter_ext/__init__.py
+++ civis_jupyter_ext/__init__.py
@@ -1,5 +1,13 @@
+try:
+    from importlib.metadata import version
+except ModuleNotFoundError:
+    # For Python 3.7
+    from importlib_metadata import version
+
 from .magics import load_ipython_extension  # noqa: F401
-from .version import __version__  # noqa: F401
+
+
+__version__ = version("civis-jupyter-extensions")
 
 
 def _jupyter_nbextension_paths():
